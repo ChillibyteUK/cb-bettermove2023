5ddd4e70e2d15b3e80559528fb8347a861ff1e79
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-08-16 17:30:59 +0100

    blog bits

--- index.php
+++ index.php
@@ -1,121 +1,215 @@
 <?php
+/**
+ * Template Name: Blog Index
+ */
+
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 
-$page_for_posts = get_option( 'page_for_posts' );
-$bg = get_the_post_thumbnail_url($page_for_posts,'full');
-
-
-
 get_header();
+$img = get_the_post_thumbnail_url(get_option( 'page_for_posts' ),'full');
 ?>
 <main id="main">
-<!-- hero -->
-<section id="hero" class="hero d-flex align-items-center hero--default">
-    <div class="overlay"></div>
-    <div class="hero__inner container-xl">
-        <div class="row h-100">
-            <div class="col-lg-6 hero__content d-flex flex-column justify-content-center order-2 order-lg-1 py-5" data-aos="fade">
-                <h1><?=get_the_title($page_for_posts)?></h1>
+<section class="hero" style="background-image:url(<?=$img?>);">
+    <div class="container-xl">
+        <div>
+            <div class="preTitle text-white">Read our latest posts</div>
+            <h1>
+                The Bettermove Blog
+            </h1>
+        </div>
+    </div>
+</section>
+<section class="has-secondary-200-background-color mb-5">
+    <div class="container-xl py-5">
+        <div class="row g-5">
+            <div class="col-lg-9 order-2 order-lg-1">
+                <div class="h2 text--black mb-4">Latest News</div>
+                <div class="row g-4">
+            <?php
+            $w = new WP_Query( array (
+                'post_type' => 'post',
+                'post_status' => 'publish',
+                'posts_per_page' => 2,
+                'orderby' => 'publish_date',
+                'order' => 'DESC',
+            ));
+            while ($w->have_posts()) {
+                $w->the_post();
+                $categories = get_the_category();
+                ?>
+                <div class="col-md-6 index_blog">
+                    <a class="index_blog__card" href="<?=get_the_permalink()?>">
+                        <img class="index_blog__image" src="<?=get_the_post_thumbnail_url($w->ID, 'large')?>">
+                        <div class="index_blog__content">
+                            <?php
+                            if ( ! empty( $categories ) ) {
+                                ?>
+                                <div class="index_blog__cat"><?=esc_html( $categories[0]->name )?></div>
+                                <?php
+                            }
+                            ?>
+                            <h2 class="index_blog__title pb-2"><?=get_the_title()?></h2>
+                            <div class="d-flex flex-column">
+                                <div class="index_blog__read"><?=estimate_reading_time_in_minutes(get_the_content(null, false, $w->ID), 200, true, false)?>m read</div>
+                                <div class="index_blog__meta">
+                                    <div class="index_blog__date"><?=get_the_date('jS F, Y', $w->ID)?></div>
+                                    <div class="readmore">Read more</div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <?php
+            }
+            
+            ?>
+                    </div>
             </div>
-            <div class="col-lg-6 hero__image order-1 order-lg-2" style="background-image:url(<?=$bg?>)">
+            <div class="col-lg-3 order-1 order-lg-3 mb-4">
+                <div class="index_blog__category">
+                    <div class="h4 ff-title">By category</div>
+                    <ul class="insights-nav d-none d-lg-block">
+                    <?php
+                        $categories = get_categories();
+                    ?>
+                        <li>
+                            <a class="active" href="<?=get_post_type_archive_link( 'post' );?>">All</a>
+                        </li>
+                    <?php
+                    foreach( $categories as $cat ) {
+                        ?>
+                        <li>
+                            <a class="" href="<?=get_category_link( $cat->term_id )?>"><?=$cat->name?></a>
+                        </li>
+                        <?php
+                    }
+                    ?>
+                    </ul>
+                    <div class="d-lg-none">
+                        <select name="cat" id="category" class="form-select">
+                            <option value="/blog/">All</option>
+                            <?php
+                            foreach( $categories as $cat ) {
+                                ?>
+                            <option value="<?=get_category_link( $cat->term_id )?>"><?=$cat->name?></option>
+                                <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="overlay--bottom"></div>
 </section>
-
-    <div class="container-xl py-5">
-        <?php
-        if (get_the_content(null, false, $page_for_posts)) {
-            echo '<div class="mb-5">' . get_the_content(null, false, $page_for_posts) . '</div>';
-        }
-
-        $cats = get_categories(array('exclude' => array(32)));
+<?php
+$section = 0;
+foreach (get_categories() as $c) {
+    /*
+    if ($section == 0) {
         ?>
-        <div class="filters mb-4">
+<section class="py-5">
+    <div class="container">
+        <div class="shadow rounded-5 pb-5 heroContent bg-light">
             <?php
-        echo '<button class="btn btn-outline-primary active me-2 mb-2" data-filter="*">All</button>';
-        foreach ($cats as $cat) {
-            echo '<button class="btn btn-outline-primary me-2 mb-2" data-filter=".' . cbslugify($cat->name) . '">' . $cat->cat_name . '</button>';
-        }
-        echo '<a href="/events/" class="btn btn-outline-primary me-2 mb-2">Events</a>';
-        ?>
-        </div>
-        <div class="row w-100" id="grid">
-            <?php
-            while (have_posts()) {
-                the_post();
-                $img = get_the_post_thumbnail_url( get_the_ID(), 'large' );
-                if (!$img) {
-                    $img = get_stylesheet_directory_uri() . '/img/default-blog.jpg';
-                }
-                $cats = get_the_category();
-                $category = wp_list_pluck($cats, 'name');
-                $flashcat = cbslugify($category[0]);
-                $catclass = implode(' ', array_map( 'cbslugify', $category ) );
-                $category = implode(', ',$category);
-
-                if (has_category('event')) {
-                    $the_date = get_field('start_date',get_the_ID());
-                }
-                else {
-                    $the_date = get_the_date('jS F, Y');
-                }
-
+            $h = new WP_Query(array(
+                'post_type' => 'page',
+                'meta_query' => array(
+                    array(
+                        'key' => 'is_hero',
+                        'value' => 'Yes',
+                        'compare' => 'LIKE'
+                    )
+                )
+            ));
+            while ($h->have_posts()) {
+                $h->the_post();
                 ?>
-            <div class="grid_item col-lg-4 col-md-6 p-0 <?=$catclass?>">
-                <a href="<?=get_the_permalink(get_the_ID())?>">
-                    <div class="card card--<?=$flashcat?>">
-                        <div class="news__image_container">
-                            <div class="news__flash news__flash--<?=$flashcat?>"><?=$category?></div>
-                            <div class="news__image" style="background-image:url('<?=get_the_post_thumbnail_url(get_the_ID(),'large')?>')"></div>
-                        </div>
-                        <div class="news__inner">
-                            <h3 class="news__title mb-0"><?=get_the_title()?></h3>
-                            <div class="news__date"><?=$the_date?></div>
-                            <div class="news__content">
-                                <div class="news__content__overlay"></div>
-                                <?=wp_trim_words(get_the_content(get_the_ID()),20)?>
+                <div class="heroContent__post">
+                    <a href="<?=get_the_permalink($h->ID)?>">
+                        <div class="row bg-white">
+                            <div class="col-md-4">
+                                <div class="heroContent__image" style="background-image:url('<?=get_the_post_thumbnail_url($h->ID, 'large')?>'"></div>
+                            </div>
+                            <div class="col-md-8 heroContent__words"">
+                                <h2><?=get_the_title($h->ID)?></h2>
+                                <div class="heroContent__intro"><?=wp_trim_words(get_the_content($h->ID))?></div>
+                                <div class="readmore">Read more</div>
                             </div>
                         </div>
-                        <!-- <div class="card__link">Read more</div> -->
-                    </div>
-                </a>
-            </div>
+                    </a>
+                </div>
                 <?php
             }
             ?>
         </div>
-<!--        <div class="mt-5">
+    </div>
+</section>
         <?php
-        numeric_posts_nav();
+    }
+    */
+    ?>
+<section class="mb-5">
+    <div class="container">
+        <div class="d-flex justify-content-between">
+            <div class="h2 text--black mb-4"><?=$c->name?></div>
+            <a href="<?=get_category_link($c->term_id)?>" class="view-all">View All</a>
+        </div>
+        <div class="row g-4">
+        <?php
+        $q = get_posts(array(
+            'numberposts' => 4,
+            'category' => $c->term_id
+        ));
+        foreach ($q as $p) {
+            ?>
+            <div class="col-lg-3 col-md-6 index_blog">
+                <a class="index_blog__card" href="<?=get_the_permalink($p->ID)?>">
+                    <img class="index_blog__image" src="<?=get_the_post_thumbnail_url($p->ID, 'large')?>">
+                    <div class="index_blog__content">
+                        <h2 class="fs-5 index_blog__title pb-2 mb-0"><?=get_the_title($p->ID)?></h2>
+                        <div class="d-flex flex-column">
+                            <div class="index_blog__read"><?=estimate_reading_time_in_minutes(get_the_content(null, false, $p->ID), 200, true, false)?>m read</div>
+                            <div class="index_blog__meta">
+                                <div class="index_blog__date"><?=get_the_date('jS F, Y', $p->ID)?></div>
+                                <div class="readmore">Read more</div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <?php
+        }
         ?>
         </div>
-        -->
     </div>
-</main>
+</section>
+    <?php
+    $section++;
+}?>
+    </div>
+</div>
 <?php
-add_action('wp_footer',function(){
-?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js" integrity="sha512-Zq2BOxyhvnRFXu0+WE6ojpZLOU2jdnqbrM1hmVdGzyeCa1DgM3X5Q4A/Is9xA1IkbUeDd7755dNNI/PzSf2Pew==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+add_action('wp_footer', function(){
+    ?>
 <script>
 (function($){
-        
-    var $grid=$('#grid').isotope({
-        itemSelector:'.grid_item',
-        percentPosition: true,
-        layoutMode: 'fitRows',
-    });
-    
-    $('.filters').on('click','button',function(){
-        var filterValue=$(this).attr('data-filter');
-        $('.filters').find('.active').removeClass('active');
-        $(this).addClass('active');
-        $grid.isotope({filter:filterValue});
+    $('.form-select').on('change',function(){
+        window.location.href = this.value;
     });
 
-
+    $('.heroContent').slick({
+        dots: true,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        fade: true
+    });
 
 })(jQuery);
 </script>

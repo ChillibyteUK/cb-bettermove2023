1bb4d9eaf427751c7b140e4f9707c35f283a0272
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-08-24 12:05:40 +0100

    parameterised why us block

--- page-templates/blocks/cb_why_us.php
+++ page-templates/blocks/cb_why_us.php
@@ -1,44 +1,36 @@
 <section class="why_us py-5 bg--secondary">
     <div class="container-xl">
-        <div class="why_us__preTitle text-center">The award winning choice</div>
-        <h2 class="why_us__title text-center">Why choose us?</h2>
-        <div class="text-center mb-5">There's lots of things that set us apart. Here's just a few.</div>
+        <div class="why_us__preTitle text-center"><?=get_field('pre_title')?></div>
+        <h2 class="why_us__title text-center"><?=get_field('title')?></h2>
+        <div class="text-center mb-5"><?=get_field('intro')?></div>
         <div class="row g-5 mb-5">
             <div class="col-md-3 text-center">
                 <img class="why_us__image"
-                    src="<?=get_stylesheet_directory_uri()?>/img/bettermove-quick.svg"
+                    src="<?=wp_get_attachment_image_url(get_field('image_1'),'full')?>"
                     alt="">
-                <div class="fs-5">Quick</div>
-                <div>If you need a quick sale we can help by buying the property from you and completing in as little as
-                    7 days. If you would prefer a longer completion date then that is fine, we will tailor make a
-                    solution to suit your needs based on your timescales.</div>
+                <div class="fs-5"><?=get_field('title_1')?></div>
+                <div><?=get_field('content_1')?></div>
             </div>
             <div class="col-md-3 text-center">
                 <img class="why_us__image"
-                    src="<?=get_stylesheet_directory_uri()?>/img/bettermove-convenient.svg"
+                    src="<?=wp_get_attachment_image_url(get_field('image_2'),'full')?>"
                     alt="">
-                <div class="fs-5">Convenient</div>
-                <div>Our aim is to make selling your home easy and convenient. We've streamlined the traditionally slow
-                    and complicated Estate Agency model to make the entire experience stress free and simple. Whether
-                    you sell to us for cash or let us find you a buyer we will make the process as pain free as
-                    possible.</div>
+                <div class="fs-5"><?=get_field('title_2')?></div>
+                <div><?=get_field('content_2')?></div>
             </div>
             <div class="col-md-3 text-center">
                 <img class="why_us__image"
-                    src="<?=get_stylesheet_directory_uri()?>/img/bettermove-transparent.svg"
+                    src="<?=wp_get_attachment_image_url(get_field('image_3'),'full')?>"
                     alt="">
-                <div class="fs-5">Transparent</div>
-                <div>We will never tie you into a long-term or unfair agreement. Furthermore we are regulated by The
-                    Property Ombudsman and are also proud members of the NAPB (national association of property buyers)
-                    so you know you're in safe hands.</div>
+                <div class="fs-5"><?=get_field('title_3')?></div>
+                <div><?=get_field('content_3')?></div>
             </div>
             <div class="col-md-3 text-center">
                 <img class="why_us__image"
-                    src="<?=get_stylesheet_directory_uri()?>/img/bettermove-zero-fees.svg"
+                    src="<?=wp_get_attachment_image_url(get_field('image_4'),'full')?>"
                     alt="">
-                <div class="fs-5">Zero Fees</div>
-                <div>We don't charge any fees, and there are no hidden costs. Even when we sell your home there will be
-                    no fees or commission to pay. We will also cover your Solicitors fees when you sell*</div>
+                <div class="fs-5"><?=get_field('title_4')?></div>
+                <div><?=get_field('content_4')?></div>
             </div>
         </div>
         <div class="text-center"><a href="/about-bettermove/" class="btn btn-primary btn-arrow">Learn more</a></div>

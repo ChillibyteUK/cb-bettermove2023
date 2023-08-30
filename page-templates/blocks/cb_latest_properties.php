b2746c6afdc130afd455264d2136818a815cb3b6
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-08-30 16:07:30 +0100

    Update cb_latest_properties.php

--- page-templates/blocks/cb_latest_properties.php
+++ page-templates/blocks/cb_latest_properties.php
@@ -32,7 +32,7 @@
                         <div class="property__beds"><?=$property->bedrooms?> <?=pluralise($property->bedrooms,'Bedroom')?></div>
                         <div class="property__baths"><?=$property->bathrooms?> <?=pluralise($property->bathrooms,'Bathroom')?></div>
                     </div>
-                    <a href="<?=get_the_permalink()?>" class="btn btn-primary">More details</a>
+                    <div class="p-2 text-center"><a href="<?=get_the_permalink()?>" class="btn btn-primary">More details</a></div>
                 </div>
             </div>
         <?php

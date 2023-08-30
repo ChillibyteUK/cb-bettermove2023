1c61ecaf083d7b204e2bbbaaed89e45d3d193e87
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-08-30 14:35:13 +0100

    Update cb_latest_properties.php

--- page-templates/blocks/cb_latest_properties.php
+++ page-templates/blocks/cb_latest_properties.php
@@ -12,7 +12,7 @@
         ));
         while ($q->have_posts()) {
             $q->the_post();
-            $me = get_post($q->ID);
+            $me = get_post_meta($q->ID);
             var_dump($me);
         ?>
             <div class="col-md-4">

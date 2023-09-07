efb7f6ae0aa84bd39079da8131e90e7336bdee7d
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-09-07 17:18:34 +0100

    Update cb_tp_slider.php

--- page-templates/blocks/cb_tp_slider.php
+++ page-templates/blocks/cb_tp_slider.php
@@ -1,5 +1,9 @@
 <section class="tp_slider py-5">
-    <div class="container-xl">
-        TP Slider Widget Here
+    <div class="container-xl d-flex justify-content-around">
+        <div class="">NAPB</div>
+        <div class="">TPOS</div>
+        <div class="">GOV AML</div>
+        <div class="">AllAgents</div>
+        <div class="">TrustPilot</div>
     </div>
 </section>

c6a3a381133ad88301e79ef4659ed7a70cac5057
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-10-04 15:07:09 +0100

    related guides cards

--- single.php
+++ single.php
@@ -76,11 +76,18 @@ $img = get_the_post_thumbnail_url(get_the_ID(),'full');
             while ($r->have_posts()) {
                 $r->the_post();
                 ?>
-                <div class="col-md-6 col-xl-3">
-                    <a class="blog_card" href="<?=get_the_permalink()?>">
-                        <img src="<?=get_the_post_thumbnail_url(get_the_ID(),'large')?>" alt="" class="blog_card__image">
-                        <div class="blog_card__content">
-                            <h3 class="blog_card__title"><?=get_the_title()?></h3>
+                <div class="col-lg-3 col-md-6 index_blog">
+                    <a class="index_blog__card" href="<?=get_the_permalink(get_the_ID())?>">
+                        <img class="index_blog__image" src="<?=get_the_post_thumbnail_url(get_the_ID(), 'large')?>">
+                        <div class="index_blog__content">
+                            <h2 class="fs-5 index_blog__title pb-2 mb-0"><?=get_the_title(get_the_ID())?></h2>
+                            <div class="d-flex flex-column">
+                                <div class="index_blog__read"><?=estimate_reading_time_in_minutes(get_the_content(null, false, get_the_ID()), 200, true, false)?>m read</div>
+                                <div class="index_blog__meta">
+                                    <div class="index_blog__date"><?=get_the_date('jS F, Y', get_the_ID())?></div>
+                                    <div class="readmore">Read more</div>
+                                </div>
+                            </div>
                         </div>
                     </a>
                 </div>

419c60b4aa7b5611dd7baeca2de53abf59870c9e
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-10-05 11:13:15 +0100

    added live chat

--- footer.php
+++ footer.php
@@ -63,6 +63,21 @@ defined('ABSPATH') || exit;
     </div>
 </footer>
 <?php wp_footer();
+?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5fbe7a71a1d54c18d8ed5645/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+<?php
 if (get_field('gtm_property', 'options')) {
     ?>
 <!-- Google Tag Manager (noscript) -->

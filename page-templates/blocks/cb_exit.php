b4b47bfc1c92005683bd5f7a30726cf9eed2db8e
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-06 09:25:05 +0000

    exit popup

---
+++ page-templates/blocks/cb_exit.php
@@ -0,0 +1,33 @@
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#onExitModal">
+  Launch  modal
+</button>
+
+<div class="modal" tabindex="-1" id="onExitModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" id="onExitModalClose" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Modal body text goes here.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+add_action('wp_footer',function() {
+    ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script type="module">
+    window.bootstrap = bootstrap;
+    const myModal = new bootstrap.Modal('#onExitModal');
+    document.addEventListener("mouseout", (e) => {
+        if (!e.toElement && !e.relatedTarget) {
+           myModal.show();
+        }
+    });
+</script>
+    <?php
+});

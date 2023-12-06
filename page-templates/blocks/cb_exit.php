1abff1337221a1c7aa5bccfa77be13e3640abc41
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-06 09:35:35 +0000

    Update cb_exit.php

--- page-templates/blocks/cb_exit.php
+++ page-templates/blocks/cb_exit.php
@@ -3,7 +3,7 @@
 </button>
 
 <div class="modal" tabindex="-1" id="onExitModal">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Modal title</h5>

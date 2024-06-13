f8e75bd67c28cebc231c2809104e38e0822ec9bf
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-13 15:15:01 +0100

    Update single.php

--- single.php
+++ single.php
@@ -2,89 +2,99 @@
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 get_header();
-$img = get_the_post_thumbnail_url(get_the_ID(),'full');
+$img = get_the_post_thumbnail_url(get_the_ID(), 'full');
 ?>
 <main id="main" class="blog">
     <?php
     $content = get_the_content();
-    $blocks = parse_blocks($content);
-    $sidebar = array();
-    $after;
-    ?>
+$blocks = parse_blocks($content);
+$sidebar = array();
+$after;
+?>
     <section class="breadcrumbs container-xl">
-    <?php
-    if (function_exists('yoast_breadcrumb')) {
-        yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
-    }
-    ?>
+        <?php
+if (function_exists('yoast_breadcrumb')) {
+    yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
+}
+?>
     </section>
     <div class="container-xl">
         <div class="row g-4 pb-4">
             <div class="col-lg-9 order-2">
                 <h1 class="blog__title"><?=get_the_title()?></h1>
                 <img src="<?=$img?>" alt="" class="blog__image">
-            <?php
-            $count = estimate_reading_time_in_minutes( get_the_content(), 200, true, true );
-            echo $count;
+                <?php
+        $count = estimate_reading_time_in_minutes(get_the_content(), 200, true, true);
+echo $count;
 
-    foreach ($blocks as $block) {
-        if ($block['blockName'] == 'core/heading') {
-            if (!array_key_exists('level', $block['attrs'])) {
-                $heading = strip_tags($block['innerHTML']);
-                $id = acf_slugify($heading);
-                echo '<a id="' . $id . '" class="anchor"></a>';
-                $sidebar[$heading] = $id;
-            }
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/heading') {
+        if (!array_key_exists('level', $block['attrs'])) {
+            $heading = strip_tags($block['innerHTML']);
+            $id = acf_slugify($heading);
+            echo '<a id="' . $id . '" class="anchor"></a>';
+            $sidebar[$heading] = $id;
         }
-        echo render_block($block);
     }
-            ?>
+    echo render_block($block);
+}
+?>
             </div>
             <div class="col-lg-3 order-1">
                 <?php
-                if ($sidebar) {
-                    ?>
+    if ($sidebar) {
+        ?>
                 <div class="sidebar">
                     <div class="h5 d-none d-lg-block">Quick Links</div>
                     <div class="h5 d-lg-none" data-bs-toggle="collapse" href="#links" role="button">Quick Links</div>
                     <div class="collapse d-lg-block" id="links">
                         <?php
-                        foreach ($sidebar as $heading => $id) {
-                            ?>
-                            <li><a href="#<?=$id?>"><?=$heading?></a></li>
-                            <?php
-                        }
-                        ?>
+            foreach ($sidebar as $heading => $id) {
+                ?>
+                        <li><a
+                                href="#<?=$id?>"><?=$heading?></a>
+                        </li>
+                        <?php
+            }
+        ?>
                     </div>
                 </div>
-                    <?php
-                }
-                ?>
+                <a href="/contacts/" class="btn btn btn-secondary btn-arrow">Let's Talk</a>
+                <?php
+    }
+?>
             </div>
         </div>
         <section class="related pb-5">
             <h3><span>Related</span> Guides</h3>
             <div class="row g-4">
-            <?php
+                <?php
             $cats = get_the_category();
-            $ids = wp_list_pluck($cats,'term_id');
-            $r = new WP_Query(array(
-                'category__in' => $ids,
-                'posts_per_page' => 4,
-                'post__not_in' => array(get_the_ID())
-            ));
-            while ($r->have_posts()) {
-                $r->the_post();
-                ?>
+$ids = wp_list_pluck($cats, 'term_id');
+$r = new WP_Query(array(
+    'category__in' => $ids,
+    'posts_per_page' => 4,
+    'post__not_in' => array(get_the_ID())
+));
+while ($r->have_posts()) {
+    $r->the_post();
+    ?>
                 <div class="col-lg-3 col-md-6 index_blog">
-                    <a class="index_blog__card" href="<?=get_the_permalink(get_the_ID())?>">
-                        <img class="index_blog__image" src="<?=get_the_post_thumbnail_url(get_the_ID(), 'large')?>">
+                    <a class="index_blog__card"
+                        href="<?=get_the_permalink(get_the_ID())?>">
+                        <img class="index_blog__image"
+                            src="<?=get_the_post_thumbnail_url(get_the_ID(), 'large')?>">
                         <div class="index_blog__content">
-                            <h2 class="fs-5 index_blog__title pb-2 mb-0"><?=get_the_title(get_the_ID())?></h2>
+                            <h2 class="fs-5 index_blog__title pb-2 mb-0">
+                                <?=get_the_title(get_the_ID())?></h2>
                             <div class="d-flex flex-column">
-                                <div class="index_blog__read"><?=estimate_reading_time_in_minutes(get_the_content(null, false, get_the_ID()), 200, true, false)?>m read</div>
+                                <div class="index_blog__read">
+                                    <?=estimate_reading_time_in_minutes(get_the_content(null, false, get_the_ID()), 200, true, false)?>m
+                                    read</div>
                                 <div class="index_blog__meta">
-                                    <div class="index_blog__date"><?=get_the_date('jS F, Y', get_the_ID())?></div>
+                                    <div class="index_blog__date">
+                                        <?=get_the_date('jS F, Y', get_the_ID())?>
+                                    </div>
                                     <div class="readmore">Read more</div>
                                 </div>
                             </div>
@@ -92,8 +102,8 @@ $img = get_the_post_thumbnail_url(get_the_ID(),'full');
                     </a>
                 </div>
                 <?php
-            }
-            ?>
+}
+?>
             </div>
         </section>
     </div>

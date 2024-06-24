6453ab2be6af555fc94c0733aac0592f7232151b
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-24 14:00:35 +0100

    hero form

--- page-templates/blocks/cb_hero.php
+++ page-templates/blocks/cb_hero.php
@@ -8,6 +8,9 @@ $img = wp_get_attachment_image_url(get_field('background'), 'full');// ?? null;
             <h1>
                 <?=get_field('title')?>
             </h1>
+            <div class="hero_form">
+                <?=do_shortcode('[gravityform id="1" title="false"]')?>
+            </div>
         </div>
     </div>
 </section>

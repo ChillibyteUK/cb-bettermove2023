701f8a3a47ab6e148bebf696656a7ec1f347d651
Matthew Pont <matthew.pont@gmail.com>
2024-11-19 10:57:52 +0000

    Update cb_latest_blogs.php

--- page-templates/blocks/cb_latest_blogs.php
+++ page-templates/blocks/cb_latest_blogs.php
@@ -1,3 +1,44 @@
+<style>
+.blog_card__image {
+    aspect-ratio: 16 / 9;
+    width: 100%;
+    -o-object-fit: cover;
+    object-fit: cover;
+    -o-object-position: center;
+    object-position: center;
+    border-top-left-radius: 1rem;
+    border-top-right-radius: 1rem;
+}
+
+.blog_card__content {
+    padding: .5rem;
+}
+
+.blog_card__title {
+    font-size: 1.1rem;
+}
+
+.blog_card {
+    background-color: var(--col-grey);
+    border-radius: 1rem;
+    height: 100%;
+    text-decoration: none;
+    display: block;
+    color: #000;
+    transition: all .3s ease-out;
+    box-shadow: rgba(0, 0, 0, .16) 0 3px 6px, rgba(0, 0, 0, .23) 0 3px 6px;
+}
+
+.has-grey-background-color {
+    background-color: var(--col-secondary-200);
+}
+
+.bg-grey--half {
+    background-image: url("data:image/svg+xml,<?xmlversion='1.0'encoding='UTF-8'?><svgid='Layer_2'data-name='Layer2'xmlns='http://www.w3.org/2000/svg'viewBox='008.47504'><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#fff0f0;}</style></defs><gid='Layer_1-2'data-name='Layer1'><rectclass='cls-2'width='8.47'height='252'/><rectclass='cls-1'y='252'width='8.47'height='252'/></g></svg>");
+    background-size: cover;
+    background-position: center;
+}
+</style>
 <!-- latest_guides -->
 <section class="latest_guides">
     <div class="has-grey-background-color">

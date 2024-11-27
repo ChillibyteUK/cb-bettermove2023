3248b59f9a98f1d841fbadc6125662c554bb4b63
Matthew Pont <matthew.pont@gmail.com>
2024-11-27 11:45:23 +0000

    Update cb_portals.php

--- page-templates/blocks/cb_portals.php
+++ page-templates/blocks/cb_portals.php
@@ -2,20 +2,20 @@
     <div class="container-xl text-center">
         <div class="preTitle">Property Portals</div>
         <h2 class="mb-5">Appearing on</h2>
-        <div class="d-flex w-100 justify-content-around align-items-center flex-wrap g-4 portalslick">
-            <div class="portals__card">
+        <div class="portalslick">
+            <div class="">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/logo-rightmove.png"
                     alt="rightmove">
             </div>
-            <div class="portals__card">
+            <div class="">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/logo-zoopla.png"
                     alt="Zoopla">
             </div>
-            <div class="portals__card">
+            <div class="">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/logo-onthemarket.png"
                     alt="OnTheMarket">
             </div>
-            <div class="portals__card">
+            <div class="">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/logo-primelocation.png"
                     alt="PrimeLocation">
             </div>

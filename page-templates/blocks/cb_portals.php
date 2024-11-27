36bec1a48d6d9ab90400d6e1b5b678ae69845f46
Matthew Pont <matthew.pont@gmail.com>
2024-11-27 11:41:26 +0000

    Update cb_portals.php

--- page-templates/blocks/cb_portals.php
+++ page-templates/blocks/cb_portals.php
@@ -2,7 +2,7 @@
     <div class="container-xl text-center">
         <div class="preTitle">Property Portals</div>
         <h2 class="mb-5">Appearing on</h2>
-        <div class="d-flex w-100 justify-content-around align-items-center flex-wrap g-4">
+        <div class="d-flex w-100 justify-content-around align-items-center flex-wrap g-4 portalslick">
             <div class="portals__card">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/logo-rightmove.png"
                     alt="rightmove">
@@ -22,3 +22,38 @@
         </div>
     </div>
 </section>
+<?php
+add_action('wp_footer', function () {
+?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"
+    integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
+    integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
+    integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script>
+    (function($) {
+        $('.portalslick').slick({
+            dots: false,
+            arrows: false,
+            infinite: true,
+            autoplay: true,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        })
+    })(jQuery);
+</script>
+<?php
+}, 9999);

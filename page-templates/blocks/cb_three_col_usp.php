e9f290dda1bfda0791743969128a6b690ce84d24
Matthew Pont <matthew.pont@gmail.com>
2025-07-02 13:02:25 +0100

    Update cb_three_col_usp.php

--- page-templates/blocks/cb_three_col_usp.php
+++ page-templates/blocks/cb_three_col_usp.php
@@ -14,23 +14,20 @@ if ( is_front_page() ) {
 }
 
 ?>
-<section class="three_col_usp pb-4">
+<section class="three_col_usp py-4">
     <div class="container-xl">
         <div class="row gx-4 gy-3 <?= esc_attr( $width ); ?> mx-auto">
             <div class="col-md-4 three_col_usp__item px-0 px-md-2">
-                <div class="three_col_usp__icon"></div>
                 <div class="three_col_usp__content <?= esc_attr( $hp ); ?>">
                     <?= wp_kses_post( $usp1 ); ?>
                 </div>
             </div>
             <div class="col-md-4 three_col_usp__item px-0 px-md-2">
-                <div class="three_col_usp__icon"></div>
                 <div class="three_col_usp__content <?= esc_attr( $hp ); ?>">
                     <?= wp_kses_post( $usp2 ); ?>
                 </div>
             </div>
             <div class="col-md-4 three_col_usp__item px-0 px-md-2">
-                <div class="three_col_usp__icon"></div>
                 <div class="three_col_usp__content <?= esc_attr( $hp ); ?>">
                     <?= wp_kses_post( $usp3 ); ?>
                 </div>
